1683c45630f6bb61270c6dd20e73f7c8a82580bb
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-22 16:26:56 +0500

    nextjs milestone2 website with custom css

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,14 +7,7 @@ export default function HomePage(){
                     <img src="landingimg.jpg" alt="solar-img" className="img1"/> 
                 <h1 className="landing-title">Empowering Your Building with <br /> Customized Solar Energy</h1>
                 </div>
-
-                <p className="landing-intro">
-                        Discover innovative solar solutions tailored to your energy needs. 
-                        Switch to renewable energy and embrace a sustainable future for your home or business.
-                </p>
-                
             </div>
-        </section>
-        
+        </section>   
     )
 }
